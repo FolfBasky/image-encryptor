224fd0e92912cbcbe174828e74a538e45b1d18b3
folfbasky <folfbasky@gmail.com>
2024-05-03 23:08:15 +0300

    add while

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
+import os
+from time import sleep
+#import random
+#import string
+
 def img_to_txt(file: str) -> list:
     'return text of the pucture'
     with open(file, 'rb') as f:
         r = f.read()
-        print(r)
     return list(r)
 
 f_key = 44
-def decriptor(img_txt: list, filename = 'file.jpg'):
+def decriptor(img_txt: list, filename:str = 'file', exp:str = 'jpg'):
     'make new dectiptor file'
     img_txt_copy = img_txt.copy()  # Создаем копию списка
     for x in range(50, len(img_txt_copy)):
         img_txt_copy[x] ^= f_key  # Применяем операцию XOR
-    with open(filename, 'wb+') as f:
+    with open('dec/'+'.'.join([filename,exp]), 'wb+') as f:
         f.write(bytes(img_txt_copy))
 
-def encryptor(img_txt: list, filename = 'file1.jpg'):
+def encryptor(img_txt: list, filename:str = 'file1', exp:str = 'jpg'):
     'encrypted a file'
     img_txt_copy = img_txt.copy()  # Создаем копию списка
     for x in range(50, len(img_txt_copy)):
         img_txt_copy[x] ^= f_key  # Применяем операцию XOR
-    with open(filename, 'wb+') as f:
+    with open('enc/'+'.'.join([filename,exp]), 'wb+') as f:
         f.write(bytes(img_txt_copy))
 
-#img_to_txt('cat.jpg')
-print(decriptor(img_to_txt('cat.jpg')))
-print(encryptor(img_to_txt('file.jpg')))
+def get_new_files(exp:str = 'jpg'):
+    'check new files in dir'
+    new_files = [i for i in [x[2] for x in os.walk('files')][0]]
+    return new_files
+
+def get_old_files(exp:str = 'jpg'):
+    'check old files in dir'
+    old_files = [i for i in [x[2] for x in os.walk('enc')][0]]
+    return old_files
+
+def check_new_files():
+    'return True if have new files'
+    return len([x.split('.') for x in get_new_files()]) > len([x.split('.') for x in get_old_files()])
+
+def get_unique_files():
+    return list(set(get_new_files())-set(get_old_files()))
+
+while True:
+    sleep(0.5)
+    if check_new_files():
+        res = get_unique_files()
+        for el in res:
+            name, exp = el.split('.')
+            name_new = name
+            #k = 45-len(name) if len(name) <= 45 else 0
+            #name_new = name + ''.join(random.choices(''.join(string.ascii_lowercase), k = k))
+            decriptor(img_to_txt('files/'+name+'.'+exp), name_new, exp)
+            encryptor(img_to_txt('dec/'+name_new+'.'+exp), name_new, exp)
